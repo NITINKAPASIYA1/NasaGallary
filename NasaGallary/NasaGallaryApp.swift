1fa1668dbab5def8ca7f77b1e968de955735a2f9
nitin.k <nitin.k@getswipe.in>
2025-12-29 21:04:28 +0530

    Initial Commit

---
+++ NasaGallary/NasaGallaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaGallaryApp.swift
+//  NasaGallary
+//
+//  Created by Nitin Kapasiya on 29/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NasaGallaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
